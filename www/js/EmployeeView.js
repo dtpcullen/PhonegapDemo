41f6010feb62260a23fd50828d78bf73dfe9c6fe
Des Cullen <des@dcs.ie>
2013-04-23 09:29:12 +0100

    Barcode

--- www/js/EmployeeView.js
+++ www/js/EmployeeView.js
@@ -5,6 +5,7 @@ var EmployeeView = function(employee) {
         this.el.on('click', '.add-location-btn', this.addLocation);
         this.el.on('click', '.add-contact-btn', this.addToContacts);
         this.el.on('click', '.change-pic-btn', this.changePicture);
+        this.el.on('click', '.barcode-btn', this.readBarcode);
     };
 
     this.render = function() {
@@ -66,6 +67,21 @@ var EmployeeView = function(employee) {
 
         return false;
     };
+    
+    this.readBarcode = function(event) {
+      
+        window.plugins.barcodeScanner.scan( function(result) {
+                                           alert("We got a barcode\n" +
+                                                 "Result: " + result.text + "\n" +
+                                                 "Format: " + result.format + "\n" +
+                                                 "Cancelled: " + result.cancelled);
+                                           }, function(error) {
+                                           alert("Scanning failed: " + error);
+                                           }
+                                           );
+        
+    };
+    
 
     this.initialize();
 
