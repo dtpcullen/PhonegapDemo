0d84d60d44b66be2cf3ea1d8047f8656b8173301
Des Cullen <des@dcs.ie>
2013-04-23 10:23:26 +0100

    Prevent default

--- www/js/EmployeeView.js
+++ www/js/EmployeeView.js
@@ -70,18 +70,20 @@ var EmployeeView = function(employee) {
     
     this.readBarcode = function(event) {
         event.preventDefault();
-        alert("Started");
+        
         try {
             window.plugins.barcodeScanner.scan(function(args) {
-                                               console.log("Scanner result: \n" +
+                                               alert("Scanner result: \n" +
                                                            "text: " + args.text + "\n" +
                                                            "format: " + args.format + "\n" +
                                                            "cancelled: " + args.cancelled + "\n");
-                                             
+                 
+                                               document.getElementById("info").innerHTML = args.text;
+                                               console.log(args);
                                                
            });
         } catch (ex) {
-            console.log(ex.message);
+            alert(ex.message);
         }
         
     };
